f26a2f4359512c73ce57ff3f567ee9b7517416f5
Niwenjin <ni_wenjin@qq.com>
2023-06-20 19:08:38 +0800

    read key_value from file

---
+++ build/CMakeFiles/key_value.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/key_value.dir/main.cpp.o"
+  "CMakeFiles/key_value.dir/main.cpp.o.d"
+  "key_value"
+  "key_value.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/key_value.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
