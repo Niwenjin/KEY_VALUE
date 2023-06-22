d5b34268044a81ce93907b582b5fd2369d5e305b
Niwenjin <ni_wenjin@qq.com>
2023-06-22 17:46:31 +0800

    add README.md

--- main.cpp
+++ main.cpp
@@ -1,152 +1,157 @@
 #include <iostream>
-#include <fstream>
-#include <string>
 #include <unordered_map>
+#include <string>
+#include <fstream>
 #include <readline/readline.h>
 #include <readline/history.h>
 #include <getopt.h>
 
-// 声明全局的键值对存储
-std::unordered_map<std::string, std::string> keyValues;
+using namespace std;
+
+unordered_map<string, string> keyValues;
 
 // 打印帮助信息
-void printHelp() {
-    std::cout << "Usage: program [options]" << std::endl;
-    std::cout << "Options:" << std::endl;
-    std::cout << "  -f, --file <filename>   Read key-value pairs from file" << std::endl;
-    std::cout << "  -a, --add               Add a key-value pair interactively" << std::endl;
-    std::cout << "  -q, --query <key>       Query the value for a key interactively" << std::endl;
-    std::cout << "  -h, --help              Show help message" << std::endl;
+void printhelp()
+{
+    cout << "KEY_VALUE" << endl;
+    cout << "Usage:program [options]" << endl;
+    cout << "options:" << endl;
+    cout << "  -a [key=value]   Add key_value pair" << endl;
+    cout << "  -q [key]         Query value for a key" << endl;
+    cout << "  -f file          Read key_value pair from file" << endl;
+    cout << "  -d key           Delete a key_value pair" << endl;
+    cout << "  -h               Print help massage" << endl;
 }
 
-// 从文件中读取键值对
-void readKeyValueFromFile(const std::string& filename) {
-    std::ifstream file(filename);
-    if (!file) {
-        std::cerr << "Failed to open file: " << filename << std::endl;
+// 从文件读取键值对
+void readfromFile(const string &filename)
+{
+    ifstream file;
+    file.open(filename);
+    if (!file)
+    {
+        cerr << "Fail to open" << filename << endl;
         return;
     }
-
-    std::string line;
-    while (std::getline(file, line)) {
-        size_t delimiterPos = line.find('=');
-        if (delimiterPos != std::string::npos) {
-            std::string key = line.substr(0, delimiterPos);
-            std::string value = line.substr(delimiterPos + 1);
+    string line;
+    while (getline(file, line))
+    {
+        size_t pos = line.find('=');
+        if (pos != string::npos)
+        {
+            string key = line.substr(0, pos);
+            string value = line.substr(pos + 1);
             keyValues[key] = value;
         }
     }
-
     file.close();
 }
 
 // 交互式添加键值对
-void addKeyValueInteractively() {
-    std::string key, value;
-
-    char* line = readline("Enter key: ");
-    if (line) {
-        key = line;
-        add_history(line);
-        free(line);
+void addwithReadline()
+{
+    string key, value;
+    char *input = readline("Key: ");
+    if (input)
+    {
+        key = input;
+        add_history(input);
+        free(input);
     }
-
-    line = readline("Enter value: ");
-    if (line) {
-        value = line;
-        add_history(line);
-        free(line);
+    input = readline("Value: ");
+    if (input)
+    {
+        value = input;
+        add_history(input);
+        free(input);
     }
-
     keyValues[key] = value;
 }
 
-// 交互式查询键值对
-void queryKeyValueInteractively() {
-    std::string key;
-
-    char* line = readline("Enter key to query: ");
-    if (line) {
-        key = line;
-        add_history(line);
-        free(line);
-    }
-
-    auto it = keyValues.find(key);
-    if (it != keyValues.end()) {
-        std::cout << "Value for key '" << key << "': " << it->second << std::endl;
-    } else {
-        std::cout << "Key not found: " << key << std::endl;
-    }
+// 命令行参数添加键值对
+void addwithParm()
+{
 }
 
-int main(int argc, char** argv) {
-    // 命令行选项配置
-    const char* shortOptions = "f:aq:h";
-    const struct option longOptions[] = {
-        {"file", required_argument, nullptr, 'f'},
-        {"add", no_argument, nullptr, 'a'},
-        {"query", required_argument, nullptr, 'q'},
-        {"help", no_argument, nullptr, 'h'},
-        {nullptr, 0, nullptr, 0}
-    };
+// 交互式查询键值对
+void querywithReadline()
+{
+    string key;
+    char *input = readline("Key: ");
+    if (input)
+    {
+        key = input;
+        add_history(input);
+        free(input);
+    }
+    auto it = keyValues.find(key);
+    if (it != keyValues.end())
+        cout << "Value of " << key << ": " << it->second << endl;
+    else
+        cout << "Not found value of " << key << endl;
+}
 
-    // 解析命令行选项
-    int option;
-    std::string filename;
-    bool interactiveAdd = false;
-    bool interactiveQuery = false;
+// 命令行参数查询键值对
+void querywithParm()
+{
+}
 
-    while ((option = getopt_long(argc, argv, shortOptions, longOptions, nullptr)) != -1) {
-        switch (option) {
-            case 'f':
-                filename = optarg;
-                break;
-            case 'a':
-                interactiveAdd = true;
-                break;
-            case 'q':
-                interactiveQuery = true;
-                break;
-            case 'h':
-                printHelp();
-                return 0;
-            default:
-                printHelp();
-                return 1;
+// 删除键值对
+void deleteKey(const string &key)
+{
+    auto n = keyValues.erase(key);
+    if (n)
+        cout << "delete key: " << key << " successfully" << endl;
+    else
+        cout << "Key: " << key << " not exist" << endl;
+}
+
+// 存储键值对到文件
+void savetoFile()
+{
+    ofstream file("../backup.txt");
+    if (!file)
+    {
+        std::cerr << "Failed to open file: backup.txt" << endl;
+        return;
+    }
+    for (const auto &kv : keyValues)
+    {
+        file << kv.first << "=" << kv.second << endl;
+    }
+    file.close();
+}
+
+int main(int argc, char **argv)
+{
+    const char *options = "aqf:d:h";
+    string filename;
+    int opt;
+    readfromFile("../backup.txt");
+    while ((opt = getopt(argc, argv, options)) != -1)
+    {
+        switch (opt)
+        {
+        case 'a':
+            addwithReadline();
+            break;
+        case 'q':
+            querywithReadline();
+            break;
+        case 'f':
+            readfromFile(optarg);
+            break;
+        case 'd':
+            deleteKey(optarg);
+            break;
+        case 'h':
+            printhelp();
+            break;
+        default:
+            cout << "use option -h to get help" << endl;
+            return 1;
         }
     }
-
-    // 从文件中读取键值对
-    if (!filename.empty()) {
-        readKeyValueFromFile(filename);
-    }
-
-    // 交互式添加与查询键值对
-    if (interactiveAdd || interactiveQuery) {
-        // 初始化readline库
-        rl_bind_key('\t', rl_insert);
-
-        while (true) {
-            char* line = readline("> ");
-            if (!line) {
-                break;
-            }
-
-            std::string command = line;
-            free(line);
-
-            if (command == "quit" || command == "exit") {
-                break;
-            } else if (interactiveAdd && command == "add") {
-                addKeyValueInteractively();
-            } else if (interactiveQuery && command == "query") {
-                queryKeyValueInteractively();
-            } else {
-                std::cout << "Unknown command" << std::endl;
-            }
-        }
-    }
-
+    savetoFile();
     return 0;
 }
